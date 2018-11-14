8f93f035dd1a2b4d5210376143cf5074f4e638f7
Rodrigo Adones <rodrigo.adonesv@gmail.com>
2018-11-14 08:23:47 -0300

    Basic Layout

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Container, Header, Left, Body, Right, Button, Icon, Title, Footer, FooterTab, Text, Content } from 'native-base';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Container>
+        <Header>
+          <Left>
+            <Button transparent>
+              <Icon name='ios-arrow-back' />
+            </Button>
+          </Left>
+          <Body>
+            <Title>App POC</Title>
+          </Body>
+          <Right>
+            <Button transparent>
+              <Icon name='ios-menu' />
+            </Button>
+          </Right>
+        </Header>
+        <Content />
+        <Footer>
+          <FooterTab>
+            <Button vertical>
+              <Icon name="ios-apps" />
+              <Text>Apps</Text>
+            </Button>
+            <Button vertical>
+              <Icon name="ios-camera" />
+              <Text>Camera</Text>
+            </Button>
+            <Button vertical active>
+              <Icon active name="ios-navigate" />
+              <Text>Navigate</Text>
+            </Button>
+            <Button vertical>
+              <Icon name="ios-person" />
+              <Text>Contact</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
